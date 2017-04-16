70cd74aa96729b8f18804dcb0d7f1bbbeec32070
Thanh Tran <python0429@gmail.com>
2017-04-15 23:57:46 -0400

    Apply .gitignore

--- build/sources/main/java/landmaster/landcraft/proxy/CommonProxy.java
+++
@@ -1,8 +0,0 @@
-package landmaster.landcraft.proxy;
-
-import net.minecraft.item.*;
-
-public class CommonProxy {
-	public void registerItemRenderer(Item item, int meta, String id) {
-	}
-}
--- build/sources/main/java/landmaster/landcraft/tile/TEPlayerMime.java
+++
@@ -1,7 +0,0 @@
-package landmaster.landcraft.tile;
-
-public class TEPlayerMime extends TEEnergy {
-	public TEPlayerMime() {
-		super(240000, 1000, 1000);
-	}
-}
