988d93f03f2afe7f315687338201bf5b7f070d87
Thanh Tran <python0429@gmail.com>
2017-04-21 23:13:21 -0400

    Bump version

--- src/main/java/landmaster/landcraft/LandCraft.java
+++ src/main/java/landmaster/landcraft/LandCraft.java
@@ -21,7 +21,7 @@ import net.minecraftforge.oredict.*;
 public class LandCraft {
 	public static final String MODID = "landcraft";
 	public static final String NAME = "Land Craft";
-	public static final String VERSION = "1.0.0.0";
+	public static final String VERSION = "1.0.0.1";
 	public static final String DEPENDS = "required-after:landcore@[1.3.0.1,);required-after:compatlayer;after:OpenComputers";
 	
 	@Mod.Instance(MODID)
