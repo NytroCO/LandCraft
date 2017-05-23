e55712cc3bc6e33136fb18b3118ff2fe25f84976
Thanh Tran <python0429@gmail.com>
2017-05-22 20:50:09 -0400

    Lower restrictions on LandCore version

--- src/main/java/landmaster/landcraft/LandCraft.java
+++ src/main/java/landmaster/landcraft/LandCraft.java
@@ -34,7 +34,7 @@ public class LandCraft {
 	public static final String MODID = "landcraft";
 	public static final String NAME = "Land Craft";
 	public static final String VERSION = "1.2.0.0";
-	public static final String DEPENDS = "required-after:landcore@[1.3.4.0,);required-after:compatlayer@[0.2.8,);"
+	public static final String DEPENDS = "required-after:landcore@[1.3.3.0,);required-after:compatlayer@[0.2.8,);"
 			+ "after:OpenComputers;after:opencomputers;after:JEI;after:jei";
 	
 	@Instance(MODID)
