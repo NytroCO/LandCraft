182c730f61526acfd2a7537924d5cd58acdbf623
Landmaster <python0429@gmail.com>
2017-06-17 14:28:49 -0400

    fsck dependency strings

--- src/main/java/landmaster/landcraft/LandCraft.java
+++ src/main/java/landmaster/landcraft/LandCraft.java
@@ -41,8 +41,8 @@ public class LandCraft {
 	public static final String MODID = "landcraft";
 	public static final String NAME = "Land Craft";
 	public static final String VERSION = "2.0.0.1";
-	public static final String DEPENDS = "required-after:landcore@[1.4.0.0,)"
-			+ "after:OpenComputers;after:opencomputers;after:JEI;after:jei";
+	public static final String DEPENDS = "required-after:landcore@[1.4.0.0,);"
+			+ "after:JEI;after:jei";
 	
 	@Instance(MODID)
 	public static LandCraft INSTANCE;
